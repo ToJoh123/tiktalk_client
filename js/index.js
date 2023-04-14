adafde7a69310a59e8030b0c4da943bc86f862de
Tom Johansson <tom.johansson123@gmail.com>
2023-04-14 05:33:52 +0200

    Added function to render rootComments from data

--- js/index.js
+++ js/index.js
@@ -20,4 +20,44 @@ submitBtn.addEventListener("click", (e) => {
 
 //end of publish comment button
 
+//get the comments from the data
+//TODO: replace with get request
+//filter the comments to get the comments with parentId null
+const rootComments = commentsData.filter(
+  (comment) => comment.parentId === null
+);
+//get the replies with the parent id
+const getReplies = (commentId) =>
+  comments.filter((comment) => comment.parentId === commentId);
+
+//get the comments container
+const commentsContainer = document.querySelector("#comments-container");
+// the html  for each comment
+function renderComments(comment) {
+  const commentHtml = `
+    <div class="comments" id="commentId-${comment.id}">
+        <p>${comment.text}</p>
+    
+        <div class="buttons">
+            <button class="fa-regular fa-heart"></button>
+            <button class="fa-regular fa-comment"></button>
+        </div>
+        <input
+        type="text"
+        name="comment"
+        id="comment"
+        placeholder="Write a comment..."
+        />
+    </div>
+    `;
+
+  //append the comment to the comments container
+  commentsContainer.innerHTML += commentHtml;
+}
+
+//render the comments
+rootComments.forEach((comment) => {
+  renderComments(comment);
+});
+
 console.log(commentsData);
