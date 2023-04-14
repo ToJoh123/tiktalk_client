372dba855a971fe9b791eba4006ffbfeee317a93
Tom Johansson <tom.johansson123@gmail.com>
2023-04-14 05:07:25 +0200

    Added event to publish BTN (console.log(input)

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
 import { commentsData } from "../data/data.js";
 
+// publish comment button
+
+//find submitBtn
+const submitBtn = document.querySelector("#submit-btn");
+
+//add event to submitBtn
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  //get the value of the id of the comment
+  const commentId = document.querySelector("#comment-body").value;
+
+  //TODO:replace with post request
+  console.log("this text is submitted by publish button:", commentId);
+
+  // reset the value of comment body
+  document.querySelector("#comment-body").value = "";
+});
+
+//end of publish comment button
+
 console.log(commentsData);
