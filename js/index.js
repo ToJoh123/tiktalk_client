831222163cfe000d61224492cd7bcd6932b9c894
Tom Johansson <tom.johansson123@gmail.com>
2023-04-20 15:41:36 +0200

    Separed the code to modules

--- js/index.js
+++ js/index.js
@@ -1,76 +1,6 @@
-/**
- * Varför kan jag inte importera funktionerna från andra filer?
- * @KANNÅGONFÖRKLARA
- * @BESVIKEN
- */
-
-function getAllComments() {
-  return fetch("http://localhost:3000/comments", {
-    method: "GET",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      return data.data;
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-}
-function renderComments(comments) {
-  //check if comments are empty
-  if (comments.length === 0) {
-    document.getElementById("comments-container-1").innerHTML =
-      "No comments to show";
-    return;
-  }
-  const element = document.getElementById("comments-container-1");
-  console.log(comments);
-  const commentHtml = `
-      <div class="comment">
-        <div class="commentProfile">
-          <img src="https://picsum.photos/40/40" alt="pfp" />
-          <h3>TEST FOR YOU</h3>
-        </div>
-        <p>TEST FOR YOU</p>
-        <div class="buttons">
-          <button class="fa-regular fa-heart"></button>
-          <button class="fa-regular fa-comment"></button>
-        </div>
-        <input
-          type="text"
-          name="comment"
-          id="comment"
-          placeholder="Write a comment..."
-        />
-      </div>
-    `;
-  //this should clear the comments container before adding new comments
-  element.innerHTML = "";
-  comments.forEach((comment) => {
-    element.innerHTML += commentHtml;
-  });
-}
-function getFollowerComments() {
-  return fetch("http://localhost:3000/comments/following", {
-    method: "GET",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      return data.data;
-    })
-    .catch((error) => {
-      console.error(error);
-      return [];
-    });
-}
+import { getAllComments } from "./index/getAllComments.js";
+import { getFollowerComments } from "./index/getFollowerComments.js";
+import { renderComments } from "./index/renderComments.js";
 
 function checkAuthentication() {
   const jwt = Cookies.get("jwt");
@@ -93,7 +23,7 @@ async function updateUserInfo() {
     if (!response.ok) {
       //Username value is invalid redirect.
       window.location.href = "../html/login.html";
-}
+    }
 
     const data = await response.json();
     const userFullnameElem = document.querySelectorAll(".user-fullname");
