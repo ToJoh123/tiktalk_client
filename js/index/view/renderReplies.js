45f3e7872ef8be22201fc8dbf7f03e259c1e1ad0
Tom Johansson <tom.johansson123@gmail.com>
2023-04-21 05:40:37 +0200

    Moved files and added subfolders

---
+++ js/index/view/renderReplies.js
@@ -0,0 +1,27 @@
+export function renderReplies(reply) {
+  // Get the reply container element
+  const replyElement = document.getElementById(
+    `reply-container-${reply.parentId}`
+  );
+  if (!replyElement) {
+    console.error(
+      "@renderReplies(): Reply container not found for id: ",
+      reply._id
+    );
+    return;
+  }
+  const replyHtmlContent = `
+    <div class="comment" id="comment-id-${reply._id}">
+      <div class="commentProfile">
+        <img src="https://picsum.photos/40/40" alt="pfp" />
+        <h3>${reply.username}</h3>
+      </div>
+      <p>${reply.text}</p>
+      <div class="buttons">
+        <button class="fa-regular fa-heart"></button>
+        <button class="fa-regular fa-comment"></button>
+      </div>
+    </div>
+  `;
+  replyElement.innerHTML += replyHtmlContent;
+}
