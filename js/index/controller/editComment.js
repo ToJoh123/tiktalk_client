67f257f8fdb1f93159174a02eeabb1ef599ca9d3
Tom Johansson <tom.johansson123@gmail.com>
2023-04-24 14:13:14 +0200

    Added new functions and events

--- js/index/controller/editComment.js
+++ js/index/controller/editComment.js
@@ -1,52 +1,61 @@
 export function editComment(comment) {
-  const paragrafElement = document.getElementById(
-    `comment-text-${comment._id}`
-  );
-  //turns this element into a textarea
-  paragrafElement.outerHTML = `<textarea id="comment-text-${comment._id}" class="comment-text">${comment.text}</textarea>`;
-  //adds a button to save the comment
-  const buttonElement = document.getElementById(
-    `comment-button-${comment._id}`
-  );
-  buttonElement.outerHTML = `<button id="save-edit-button-${comment._id}" class="comment-button">Save</button>`;
-  //adds a button to cancel the edit
-  const cancelButtonElement = document.getElementById(
-    `cancel-edit-button-${comment._id}`
-  );
-  cancelButtonElement.outerHTML = `<button id="cancel-edit-button-${comment._id}" class="comment-button">Cancel</button>`;
-  //adds event listener to the save button
-  const saveButtonElement = document.getElementById(
-    `save-edit-button-${comment._id}`
-  );
-  saveButtonElement.addEventListener("click", () => {
-    saveComment(comment);
-  });
-  //adds event listener to the cancel button
-  const cancelEditButtonElement = document.getElementById(
-    `cancel-edit-button-${comment._id}`
-  );
-  cancelEditButtonElement.addEventListener("click", () => {
-    cancelEditComment(comment);
-  });
-
-  // console.log("edit comment", comment);
+  convertToTextArea(comment);
+  addSaveButton(comment);
 }
-
-function saveComment(comment) {
-  const paragrafElement = document.getElementById(
-    `comment-text-${comment._id}`
+function convertToTextArea(comment) {
+  const textToEdit = document.getElementById(`comment-text-${comment._id}`);
+  textToEdit.outerHTML = `<textarea id="comment-text-${comment._id}" class="comment-text">${comment.text}</textarea>`;
+}
+function addSaveButton(comment) {
+  //check if there is already a save button
+  const saveButton = document.getElementById(`save-edit-button-${comment._id}`);
+  if (!saveButton) {
+    const buttonsContainer = document.getElementById(`buttons-${comment._id}`);
+    if (buttonsContainer) {
+      const buttonHTML = `<button id="save-edit-button-${comment._id}" class="comment-button">Save</button>
+                          <button id="cancel-edit-button-${comment._id}" class="comment-button">Cancel</button>`;
+      buttonsContainer.insertAdjacentHTML("afterend", buttonHTML);
+      const newSaveButton = document.getElementById(
+        `save-edit-button-${comment._id}`
+      );
+      newSaveButton.addEventListener("click", () => saveComment(comment));
+      const cancelEditButton = document.getElementById(
+        `cancel-edit-button-${comment._id}`
+      );
+      cancelEditButton.addEventListener("click", () => cancelEdit(comment));
+    }
+  }
+}
+function cancelEdit(comment) {
+  //reset document to original state
+  const textToEdit = document.getElementById(`comment-text-${comment._id}`);
+  textToEdit.outerHTML = `<p id="comment-text-${comment._id}" class="comment-text">${comment.text}</p>`;
+  const saveButton = document.getElementById(`save-edit-button-${comment._id}`);
+  saveButton.remove();
+  const cancelButton = document.getElementById(
+    `cancel-edit-button-${comment._id}`
   );
-  const text = paragrafElement.value;
-  const data = {
-    text: text,
-  };
-  fetch(`/api/comments/${comment._id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  }).then(() => {
-    location.reload();
-  });
+  cancelButton.remove();
+}
+function saveComment(comment) {
+  const value = document.getElementById(`comment-text-${comment._id}`).value;
+  //this code copies the comment and adds the new text to it
+  const newComment = { ...comment, text: value };
+  console.log("this is the comment", newComment);
+  // const paragrafElement = document.getElementById(
+  //   `comment-text-${comment._id}`
+  // );
+  // const text = paragrafElement.value;
+  // const data = {
+  //   text: text,
+  // };
+  // fetch(`/api/comments/${comment._id}`, {
+  //   method: "PUT",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(data),
+  // }).then(() => {
+  //   location.reload();
+  // });
 }
