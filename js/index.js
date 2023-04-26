2a204f7e2ba8d1592ceab38deddcba425d6b0dfd
Tom Johansson <tom.johansson123@gmail.com>
2023-04-26 06:46:33 +0200

    Deleted unused imports and unused function

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
 import { getAllComments } from "./index/controller/getAllComments.js";
-import { getCurrentUserComments } from "./index/controller/getCurrentUserComments.js";
 import { getFollowerComments } from "./index/controller/getFollowerComments.js";
 import { renderComments } from "./index/model/renderComments.js";
 import { postComment } from "./index/model/postComment.js";
 import { populateFollowList } from "./index/controller/populateFollowList.js";
-import { renderButtons } from "./index/model/renderButtons.js";
 import { checkAuthentication } from "./index/controller/checkAuthentication.js";
 import { updateUserInfo } from "./index/controller/updateUserInfo.js";
 import { showLogoutConfirmation } from "./index/controller/showLogoutConfirmation.js";
@@ -14,10 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
   checkAuthentication();
   updateUserInfo();
   getAllComments().then((data) => commentSection(data));
-  //delay
-  // setTimeout(() => {
-  //   getCurrentUserComments().then((data) => renderButtons(data));
-  // }, 1000);
 
   document
     .getElementById("forYouTab")
