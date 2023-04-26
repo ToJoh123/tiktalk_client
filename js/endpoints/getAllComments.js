258d45b8533618da966c265d6e61b250b7cd3d28
Tom Johansson <tom.johansson123@gmail.com>
2023-04-26 09:24:52 +0200

    Moved file to /endpoints

---
+++ js/endpoints/getAllComments.js
@@ -0,0 +1,18 @@
+// getAllComments.js
+
+export function getAllComments() {
+  return fetch("http://localhost:3000/comments", {
+    method: "GET",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data.data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
