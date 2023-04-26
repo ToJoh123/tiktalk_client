ad4faa98bea49246ae71fb4bfe206e8caf28b692
Tom Johansson <tom.johansson123@gmail.com>
2023-04-26 09:25:18 +0200

    Moved file to /endpoint

--- js/endpoints/postComment.js
+++ js/endpoints/postComment.js
@@ -1,24 +1,21 @@
-export function postComment(e) {
-  e.preventDefault();
-  const commentText = document.getElementById("post-comment-text").value;
+export function postComment(text, commentId) {
   fetch("http://localhost:3000/comments", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      text: commentText,
+      parentId: commentId,
+      text: text,
     }),
     credentials: "include",
   })
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      getAllComments().then((data) => renderComments(data));
+      // getAllComments().then((data) => renderComments(data));
     })
     .catch((error) => {
       console.error(error);
     });
-  //reset the form
-  document.getElementById("post-comment-form").reset();
 }
