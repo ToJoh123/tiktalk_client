53c556a083fd5ea28d8061531b18e3e59c3fd6c6
Tom Johansson <tom.johansson123@gmail.com>
2023-04-26 06:52:46 +0200

    Added conditional rendering to buttons

--- js/index/sections/html/replySection.js
+++ js/index/sections/html/replySection.js
@@ -1,4 +1,10 @@
-export function replySection(reply) {
+/**
+ * This code is responsible for creating the HTML element for a reply.
+ * @param {*} reply
+ * @param {*} editAbleCommentIds
+ * @returns HTML element
+ */
+export function replySection(reply, editAbleCommentIds) {
   const replyElement = `
     <div class="comment" id="comment-id-${reply._id}">
       <div class="commentProfile">
@@ -8,6 +14,14 @@ export function replySection(reply) {
       <p id="comment-text-${reply._id}">${reply.text}</p>
       <div class="buttons" id="buttons-${reply._id}">
         <button class="fa-regular fa-heart"></button>
+        ${
+          editAbleCommentIds.includes(reply._id)
+            ? `
+        <button class="fa-regular fa-heart" id="edit-btn-${reply._id}">edit</button>
+        <button class="fa-regular fa-heart" id="delete-btn-${reply._id}">delete</button>
+        `
+            : ""
+        } 
       </div>
     </div>
   `;
