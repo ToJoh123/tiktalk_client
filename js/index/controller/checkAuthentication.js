d7c457a36384f2bd2fed0b631b2432a38fc6381f
Lucas Larsson <lucas.larsson93@hotmail.com>
2023-04-26 22:34:32 +0200

    checkAuth fetch from an endpoint to validate jwt.

--- js/index/controller/checkAuthentication.js
+++ js/index/controller/checkAuthentication.js
@@ -1,6 +1,12 @@
 export function checkAuthentication() {
-    const jwt = Cookies.get("jwt");
-    if (!jwt) {
-      window.location.href = "../html/login.html";
-    }
+  //Verify the token on the serverside instead of the client.
+  fetch('/access/verify')
+  .then(response => {
+    if (response.status === 401) {
+      window.location.replace('login.html');
+    } 
+  })
+  .catch(error => {
+    console.error('Error verifying token:', error);
+  });
   }
