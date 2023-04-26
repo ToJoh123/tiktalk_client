ac44b06eddaeda28310112b7735a0cf6d1c58143
Tom Johansson <tom.johansson123@gmail.com>
2023-04-26 06:52:07 +0200

    Imported getCurrentUserComments Changed commentSection() so can return buttons if current user

--- js/index/sections/commentSection.js
+++ js/index/sections/commentSection.js
@@ -1,37 +1,57 @@
+/**
+ * this function is responsible for rendering the comment section
+ * 1. starts with a fetch to get the current user's comments
+ * 2. then it filters the comments to get the root comments
+ * 3. then it gets the replies for each root comment
+ * 4. then it gets the number of replies for each root comment
+ */
 import { rootCommentSection } from "./html/rootCommentSection.js";
+import { getCurrentUserComments } from "../../endpoints/getCurrentUserComments.js";
 const commentContainerElement = document.querySelector("#comments-container-1");
 
 export function commentSection(comments) {
-  const rootComments = comments.filter((comment) => comment.parentId === null);
-  const getReplies = (commentId) => {
-    //get replies for a comment
-    return (
-      comments
-        .filter((comment) => comment.parentId === commentId)
-        //sort replies by date --> latest first
-        .sort((a, b) => {
-          return new Date(b.createdAt) - new Date(a.createdAt);
-        })
+  getCurrentUserComments().then((data) => {
+    const backEndCurrentUserComments = data;
+    const editAbleCommentIds = backEndCurrentUserComments.map(
+      (comment) => comment._id
     );
-  };
-  const numberOfReplies = (commentId) => {
-    //get number of replies for a comment
-    return comments.filter((comment) => comment.parentId === commentId).length;
-  };
-
-  if (comments.length === 0) {
-    commentContainerElement.textContent = "No comments yet";
-  }
-  if (comments.length > 0) {
-    commentContainerElement.innerHTML = "";
-    rootComments.map((comment) => {
-      const commentElement = rootCommentSection(
-        comment,
-        numberOfReplies(comment._id),
-        getReplies(comment._id)
+    const rootComments = comments
+      .filter((comment) => comment.parentId === null)
+      .sort((a, b) => {
+        return new Date(b.createdAt) - new Date(a.createdAt);
+      });
+    const getReplies = (commentId) => {
+      //get replies for a comment
+      return (
+        comments
+          .filter((comment) => comment.parentId === commentId)
+          //sort replies by date --> latest first
+          .sort((a, b) => {
+            return new Date(b.createdAt) - new Date(a.createdAt);
+          })
       );
+    };
+    const numberOfReplies = (commentId) => {
+      //get number of replies for a comment
+      return comments.filter((comment) => comment.parentId === commentId)
+        .length;
+    };
 
-      commentContainerElement.appendChild(commentElement);
-    });
-  }
+    if (comments.length === 0) {
+      commentContainerElement.textContent = "No comments yet";
+    }
+    if (comments.length > 0) {
+      commentContainerElement.innerHTML = "";
+      rootComments.map((comment) => {
+        const commentElement = rootCommentSection(
+          comment,
+          numberOfReplies(comment._id),
+          getReplies(comment._id),
+          editAbleCommentIds
+        );
+
+        commentContainerElement.appendChild(commentElement);
+      });
+    }
+  });
 }
