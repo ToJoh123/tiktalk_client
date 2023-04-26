70736803398481762c69da1a8d0871136ddff8b6
Tom Johansson <tom.johansson123@gmail.com>
2023-04-26 12:35:49 +0200

    Moved code and imported them

--- js/index/sections/commentSection.js
+++ js/index/sections/commentSection.js
@@ -4,13 +4,18 @@
  * 2. then it filters the comments to get the root comments
  * 3. then it gets the replies for each root comment
  * 4. then it gets the number of replies for each root comment
+ * 5. keeps track if all the comments have been rendered
+ * 6. then adds the event listeners
  */
 import { rootCommentSection } from "./html/rootCommentSection.js";
 import { getCurrentUserComments } from "../../endpoints/getCurrentUserComments.js";
-import { getAllComments } from "../../endpoints/getAllComments.js";
-import { postComment } from "../../endpoints/postComment.js";
-import { deleteComment } from "../../endpoints/deleteComment.js";
-import { editComment } from "./events/editComment.js";
+import {
+  addReplyButtonListeners,
+  addSubmitFormButtonListeners,
+  addLikeButtonListeners,
+  addDeleteButtonListeners,
+  addEditButtonListeners,
+} from "./events/events.js";
 const commentContainerElement = document.querySelector("#comments-container-1");
 
 export function commentSection(comments) {
@@ -64,76 +69,11 @@ export function commentSection(comments) {
       isContentRendered = true;
     }
     if (isContentRendered) {
-      // Add event listeners to reply buttons
-      const replyButtons = document.querySelectorAll(".toggleReplies");
-      replyButtons.forEach((button) => {
-        button.addEventListener("click", function () {
-          const commentId = button.id.replace("replyBtn-", "");
-          const replyContainer = document.getElementById(
-            `reply-container-${commentId}`
-          );
-          replyContainer.classList.toggle("hidden");
-        });
-      });
-
-      // Add event listeners to submit form buttons
-      const submitFormButtons = document.querySelectorAll(
-        '[id^="submitFormBtn-"]'
-      );
-      submitFormButtons.forEach((button) => {
-        button.addEventListener("click", function (event) {
-          event.preventDefault();
-          const commentId = button.id.replace("submitFormBtn-", "");
-          const replyText = document.getElementById(
-            `post-reply-text-${commentId}`
-          );
-          // Handle the form submission logic here
-          postComment(replyText.value, commentId);
-          // Clear the form
-          document.getElementById(`post-reply-form-${commentId}`).reset();
-        });
-      });
-
-      // Add event listeners to like buttons
-      const likeBtns = document.querySelectorAll(".likeBtn");
-      likeBtns.forEach((button) => {
-        button.addEventListener("click", function () {
-          const commentid = button.id.replace("like-btn-", "");
-
-          //handle like button logic here
-          console.log("you have liked comment with id: " + commentid);
-        });
-      });
-
-      //add event listeners to delete buttons
-      const deleteBtns = document.querySelectorAll('[id^="delete-btn-"]');
-      deleteBtns.forEach((button) => {
-        button.addEventListener("click", function () {
-          const commentId = button.id.replace("delete-btn-", "");
-          //handle delete button logic here
-          deleteComment(commentId)
-            .then((data) => {
-              console.log(data);
-              window.location.reload();
-            })
-            .catch((error) => {
-              console.error(error);
-            });
-        });
-      });
-
-      //add event listeners to edit buttons
-      const editBtns = document.querySelectorAll('[id^="edit-btn-"]');
-      editBtns.forEach((button) => {
-        button.addEventListener("click", function () {
-          const commentId = button.id.replace("edit-btn-", "");
-          //handle edit button logic here
-          console.log(
-            "you have clicked the edit button for comment: " + commentId
-          );
-          editComment(commentId);
-        });
-      });
-    }
+      addReplyButtonListeners();
+      addSubmitFormButtonListeners();
+      addLikeButtonListeners();
+      addDeleteButtonListeners();
+      addEditButtonListeners();
+    } //end of events
   });
 }
