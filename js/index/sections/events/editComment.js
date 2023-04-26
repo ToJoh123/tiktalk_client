14d0992706546c65f3e8a18540ca5cf49317e075
Tom Johansson <tom.johansson123@gmail.com>
2023-04-26 14:39:23 +0200

    Fixed issue with edit event not working

--- js/index/sections/events/editComment.js
+++ js/index/sections/events/editComment.js
@@ -1,7 +1,7 @@
-// import { patchcommentIdModule } from "../../../endpoints/patchcommentModule.js";
+import { patchCommentModule } from "../../../endpoints/patchCommentModule.js";
 export function editComment(commentId) {
   convertToTextArea(commentId);
-  // addSaveButton(commentId);
+  addSaveButton(commentId);
 }
 
 function convertToTextArea(commentId) {
@@ -9,47 +9,47 @@ function convertToTextArea(commentId) {
   //change the text to a text area
   textToEdit.outerHTML = `<textarea id="comment-text-${commentId}">${textToEdit.outerText}</textarea>`;
 }
-// function addSaveButton(commentId) {
-//   //check if there is already a save button
-//   const saveButton = document.getElementById(`save-edit-button-${commentId}`);
+function addSaveButton(commentId) {
+  //check if there is already a save button
+  const saveButton = document.getElementById(`save-edit-button-${commentId}`);
 
-//   if (!saveButton) {
-//     const buttonsContainer = document.getElementById(`buttons-${commentId}`);
-//     if (buttonsContainer) {
-//       const buttonHTML = `<button id="save-edit-button-${commentId}" class="commentId-button">Save</button>
-//                             <button id="cancel-edit-button-${commentId}" class="commentId-button">Cancel</button>`;
-//       buttonsContainer.insertAdjacentHTML("afterend", buttonHTML);
-//       const newSaveButton = document.getElementById(
-//         `save-edit-button-${commentId}`
-//       );
-//       newSaveButton.addEventListener("click", () => savecommentId(commentId));
-//       const cancelEditButton = document.getElementById(
-//         `cancel-edit-button-${commentId}`
-//       );
-//       cancelEditButton.addEventListener("click", () => cancelEdit(commentId));
-//     }
-//   }
-// }
-// function cancelEdit(commentId) {
-//   //reset document to original state
-//   const textToEdit = document.getElementById(`commentId-text-${commentId}`);
-//   textToEdit.outerHTML = `<p id="commentId-text-${commentId}" class="commentId-text">${commentText}</p>`;
-//   const saveButton = document.getElementById(`save-edit-button-${commentId}`);
-//   saveButton.remove();
-//   const cancelButton = document.getElementById(
-//     `cancel-edit-button-${commentId}`
-//   );
-//   cancelButton.remove();
-// }
+  if (!saveButton) {
+    const buttonsContainer = document.getElementById(`buttons-${commentId}`);
+    if (buttonsContainer) {
+      const buttonHTML = `  <button id="save-edit-button-${commentId}" >Save</button>
+                            <button id="cancel-edit-button-${commentId}">Cancel</button>`;
+      buttonsContainer.insertAdjacentHTML("afterend", buttonHTML);
+      const newSaveButton = document.getElementById(
+        `save-edit-button-${commentId}`
+      );
+      newSaveButton.addEventListener("click", () => savecommentId(commentId));
+      const cancelEditButton = document.getElementById(
+        `cancel-edit-button-${commentId}`
+      );
+      cancelEditButton.addEventListener("click", () => cancelEdit(commentId));
+    }
+  }
+}
+function cancelEdit(commentId) {
+  //reset document to original state
+  const textToEdit = document.getElementById(`comment-text-${commentId}`);
+  textToEdit.outerHTML = `<p id="comment-text-${commentId}">${textToEdit.value}</p>`;
+  const saveButton = document.getElementById(`save-edit-button-${commentId}`);
+  saveButton.remove();
+  const cancelButton = document.getElementById(
+    `cancel-edit-button-${commentId}`
+  );
+  cancelButton.remove();
+}
 
-// async function savecommentId(commentId) {
-//   const value = document.getElementById(`commentId-text-${commentId}`).value;
-//   const res = await patchcommentIdModule(commentId, value);
+async function savecommentId(commentId) {
+  const value = document.getElementById(`comment-text-${commentId}`).value;
+  const res = await patchCommentModule(commentId, value);
 
-//   // Check the status code and reload the page if it's 200
-//   if (res && res.status === 200) {
-//     location.reload();
-//   } else {
-//     console.error("Error occurred while saving the commentId");
-//   }
-// }
+  // Check the status code and reload the page if it's 200
+  if (res && res.status === 200) {
+    location.reload();
+  } else {
+    console.error("Error occurred while saving the commentId");
+  }
+}
