e6ac08c12fc46515cf5ea0714543e4241248ea9e
Tom Johansson <tom.johansson123@gmail.com>
2023-04-27 04:43:37 +0200

    Created a function to return HTML with forms

---
+++ js/profile/html/commentFormGenerator.js
@@ -0,0 +1,13 @@
+export function commentFormGenerator(comment) {
+  return `
+      <div class="comment" id="comment-id-${comment._id}">
+        <p id="status-${comment._id}"></p>
+        <form class="editForm" id="edit-form-${comment._id}">
+          <input type="text" value="${comment.text}" id="edit-comment-${comment._id}" />
+          <button type="submit" class="fa-regular fa-save" id="edit-btn-${comment._id}"></button>
+        </form>
+        <button class="fa-solid fa-trash deleteBtn" id="delete-btn-${comment._id}"></button>
+        <p>posted: ${comment.createdAt}</p>
+      </div>
+    `;
+}
